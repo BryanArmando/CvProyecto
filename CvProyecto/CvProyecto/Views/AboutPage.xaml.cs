09914b09a35b225622dfe074d71dcdc06c385ff1
BryanArmando <bryanquisaguano@gmail.com>
2022-07-30 02:01:22 -0500

    Agregar archivos de proyecto.

---
+++ CvProyecto/CvProyecto/Views/AboutPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace CvProyecto.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        ObservableCollection<FileImageSource> imageSources = new ObservableCollection<FileImageSource>();
+        public AboutPage()
+        {
+            InitializeComponent();
+            imageSources.Add("image1.jpg");
+            imageSources.Add("image2.jpg");
+            imageSources.Add("image3.jpg");
+
+            imgSlider.Images = imageSources;
+        }
+        private void Abrir_cv(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CvPage());
+        }
+    }
+}
---
+++ CvProyecto/CvProyecto/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CvProyecto.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CvProyecto.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
